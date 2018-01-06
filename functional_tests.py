12e90d7d450a872a15945aa043aa5f30b71ed7e6
Alvaro <alvaro.camilloneto@gmail.com>
2018-01-06 00:38:21 -0200

    Testes Funcionais agora checam se nós colocamos um item novo na lista

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,8 @@
-from selenium import webdriver
+import time
 import unittest
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -17,16 +20,31 @@ class NewVisitorTest(unittest.TestCase):
 
         # Ele percebe que o título da página e o cabeçalho mencionam lista de tarefas (to-do)
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Teste finalizado!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # Ele é convidado a inserir um item de tarefa
-
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholde'),
+            'Entre um item na sua lista'
+        )
         # Ele digita "Comprar penas de pavão" em uma caixa de texto
+        inputbox.send_keys('Comprar penas de pavão')
 
         # Quando ele aperta enter, a pagina é atualizada e agora a pagina lista
         # 1: Comprar penas de pavão como um item em uma lista de tarefas
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Comprar penas de pavão' for row in rows)
+        )
 
         # Ainda há uma caixa de texto e ele acrescenta "Fazer isca de pesca"
+        self.fail("Teste Encerrado!")
 
         #  A página é atualizada novamente e agora mostra dois itens na lista
 
