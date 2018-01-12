4a7223c046559bb3027a1cd2f7b1ea39e7848bea
Alvaro <alvaro.camilloneto@gmail.com>
2018-01-11 23:26:21 -0200

    Corrigindo o Lint

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,9 +1,6 @@
-from django.http import HttpRequest
-from django.test import TestCase
-from django.urls import resolve
-from django.template.loader import render_to_string
 
-from lists.views import home_page
+from django.test import TestCase
+
 from lists.models import Item
 
 
@@ -13,11 +10,12 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
-    
+
     def test_can_save_a_POST_request(self):
         response = self.client.post('/', data={'item_text': 'Um novo item na Lista'})
         self.assertIn('Um novo item na Lista', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
+
 
 class ItemModelTest(TestCase):
 
