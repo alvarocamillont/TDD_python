9e00c938db25a55cde71236a06734fc6c5bc8e65
Alvaro <alvaro.camilloneto@gmail.com>
2018-01-15 23:22:31 -0200

    Refatoração para usar pooling ao inves de um time sleep fixo

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -3,6 +3,9 @@ import time
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
+
+MAX_WAIT = 10
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -13,10 +16,18 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # João ouviu falar de uma nova aplicação online para lista de tarefas
@@ -42,7 +53,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        self.check_for_row_in_list_table('1: Comprar penas de pavão')
+        self.wait_for_row_in_list_table('1: Comprar penas de pavão')
 
         # Ainda há uma caixa de texto e ele acrescenta "Fazer isca de pesca"
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -51,8 +62,8 @@ class NewVisitorTest(LiveServerTestCase):
         time.sleep(1)
 
         #  A página é atualizada novamente e agora mostra dois itens na lista
-        self.check_for_row_in_list_table('1: Comprar penas de pavão')
-        self.check_for_row_in_list_table('2: Fazer isca de pesca')
+        self.wait_for_row_in_list_table('1: Comprar penas de pavão')
+        self.wait_for_row_in_list_table('2: Fazer isca de pesca')
 
         # João se pergunta se o site irá lembrar dessa lista. Então ele percebe que o site gerou um
         # url único para ele - há um texto explicando isso
