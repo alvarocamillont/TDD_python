81ea310b229d651d4c95753fe1072d4370709b9b
Alvaro <alvaro.camilloneto@gmail.com>
2018-01-19 23:49:38 -0200

    Refatoração para separar o template da home  e das lista de tarefas

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -11,15 +11,6 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-    def test_display_all_list_items(self):
-        Item.objects.create(text='item 1')
-        Item.objects.create(text='item 2')
-
-        response = self.client.get('/')
-
-        self.assertIn('item 1', response.content.decode())
-        self.assertIn('item 2', response.content.decode())
-
 
 class ItemModelTest(TestCase):
 
@@ -51,9 +42,24 @@ class ItemModelTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'],'/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world')
 
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
 
+
+class ListViewTest(TestCase):
+    
+    def test_display_all_items(self):
+        Item.objects.create(text='item 1')
+        Item.objects.create(text='item 2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world')
+
+        self.assertContains(response, 'item 1')
+        self.assertContains(response, 'item 2')
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world')
+        self.assertTemplateUsed(response, 'list.html')
