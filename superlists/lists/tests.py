bb375f1c125ce1b8486933ad55c1fde92c41c5b3
Alvaro <alvaro.camilloneto@gmail.com>
2018-01-23 23:37:05 -0200

    corrigindo o path para uma versão final

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -76,8 +76,6 @@ class NewListTest(TestCase):
         self.assertEqual(new_item.text, 'A new list item')
 
     def test_redirects_after_POST(self):
-        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
-        self.assertRedirects(response, '/lists/the-only-list-in-the-world')
-
-
-
+        response = self.client.post('/lists/new', data={'item_text': 'Um novo item para a lista'})
+        new_list = List.objects.first()
+        self.assertRedirects(response, f'/lists/{new_list.id}/')
