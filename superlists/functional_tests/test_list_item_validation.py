1174893e45658cca4068451eb23327a56f91cd51
Alvaro <alvaro.camilloneto@gmail.com>
2018-02-28 23:18:41 -0300

    Criado test para validação de mensagem em branco

--- superlists/functional_tests/test_list_item_validation.py
+++ superlists/functional_tests/test_list_item_validation.py
@@ -1,3 +1,6 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
 from .base import FunctionalTest
 
 
@@ -6,15 +9,30 @@ class ItemValidationTest(FunctionalTest):
     def test_cannot_add_empty_list_items(self):
         # Edith acessa a pagina principal e acidentalmente tenta submeter
         # um item vazio na lista. Ela tecla ENter na caixa de entrada vazia
+        self.browser.get(self.live_server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         # A página inicial é atualizada e há uma mensagem de erro informando
         # que itens da lista não podem estar em branco
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "Você não pode adicionar um item vazio"
+        ))
 
         # ELa tenta novamente com um texto para o item, e isso agora funciona
-
+        self.browser.find_element_by_id('id_new_item').send_keys('Compre Leite')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Compre Leite')
         # De forma perversa, ela agora decide submeter um segundo item em branco na lista
-
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
         # Ela recebe um aviso semelhante na página da lista
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "Você não pode adicionar um item vazio"
+        ))
 
         # E ela pode corrigir isso preenchendo o item com um texto
-        self.fail('Me escreva!')
+        self.browser.find_element_by_id('id_new_item').send_keys('Fazer chá')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Compre Leite')
+        self.wait_for_row_in_list_table('2: Fazer chá')
