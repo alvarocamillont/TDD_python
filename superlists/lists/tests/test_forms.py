39815fca5c11d08b7a7d1b17e3e5e1d5c83a703b
Alvaro <alvaro.camilloneto@gmail.com>
2018-03-08 23:46:19 -0300

    Testes inacabados de formulários

---
+++ superlists/lists/tests/test_forms.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+
+from lists.forms import ItemForm
+
+
+class ItemFormTest(TestCase):
+
+    def test_form_renders_item_iput_has_placeholder_and_css_classes(self):
+        form = ItemForm()
+        self.assertIn('placeholder="Digite um item a fazer"', form.as_p())
+        self.assertIn('class="form-control input-lg"', form.as_p())
+
+    def test_form_validation_for_blank_items(self):
+        form = ItemForm(data={'text': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['text'],
+            ['Você não pode adicionar um item vazio']
+        )
