3b33dbd322a12c732469e523196ff95f470b40f9
Alvaro <alvaro.camilloneto@gmail.com>
2018-03-21 23:47:33 -0300

    Incluido testes funcionais com javascript.

--- superlists/functional_tests/test_list_item_validation.py
+++ superlists/functional_tests/test_list_item_validation.py
@@ -6,6 +6,9 @@ from .base import FunctionalTest
 
 class ItemValidationTest(FunctionalTest):
 
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
+
     def test_cannot_add_empty_list_items(self):
         # Edith acessa a pagina principal e acidentalmente tenta submeter
         # um item vazio na lista. Ela tecla ENter na caixa de entrada vazia
@@ -55,7 +58,25 @@ class ItemValidationTest(FunctionalTest):
 
         # Ela vê uma mensagem de erro
         self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector('.has-error').text,
+            self.get_error_element().text,
             "Você já inseriu esse item na sua lista."
         ))
 
+    def test_error_message_are_clear_on_input(self):
+        # Edna inicia uma lista e provoca um erro de validação
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('Fazer massa de pão.')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Fazer massa de pão.')
+        self.get_item_input_box().send_keys('Fazer massa de pão.')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for(lambda: self.assertTrue(
+            self.get_error_element().is_displayed()
+        ))
+        # Ela come a digitar na caixa para limpar o erro
+        self.get_item_input_box().send_keys('a')
+
+        # Ela fica satisfeita ao ver que a mensagem de erro desaparece
+        self.wait_for(lambda: self.assertFalse(
+            self.get_error_element().is_displayed()
+        ))   
