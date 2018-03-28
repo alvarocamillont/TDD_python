aa5268a1317dc758e2e8e65e35e4016970dc907a
Alvaro <alvaro.camilloneto@gmail.com>
2018-03-28 00:02:44 -0300

    Inicializando o tratamento do Login. Login sem senha!

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -13,11 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path, include
-from lists import views as list_views
+from django.urls import include, path
+
+from accounts import urls as accounts_urls
 from lists import urls as list_urls
+from lists import views as list_views
 
 urlpatterns = [
     path('', list_views.home_page, name='home'),
     path('lists/', include(list_urls)),
+    path('accounts/', include(accounts_urls)),
 ]
