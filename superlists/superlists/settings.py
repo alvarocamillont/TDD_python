dcc5de51b449a29325b86d4b84677b99d0bc135a
Alvaro <alvaro.camilloneto@gmail.com>
2018-04-09 23:57:58 -0300

    Configuração de Logging

--- superlists/superlists/settings.py
+++ superlists/superlists/settings.py
@@ -134,3 +134,20 @@ AUTH_USER_MODEL = 'accounts.ListUser'
 AUTHENTICATION_BACKENDS = [
     'accounts.authentication.PasswordLessAuthenticationBackend'
 ]
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler'
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+    },
+    'root': {'level': 'INFO'},
+}
