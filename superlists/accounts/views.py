05ba7c5d4680151e6fdeddf2c3e2c433180d82fa
Alvaro <alvaro.camilloneto@gmail.com>
2018-04-18 23:10:03 -0300

    Realizando tesde de classes que usam objetos externos

--- superlists/accounts/views.py
+++ superlists/accounts/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import redirect
 from django.core.mail import send_mail
 
-# Create your views here.
+
 def send_login_email(request):
+    email = request.POST['email']
+
+    send_mail(
+        'Seu login para a superlista',
+        'body text tbc',
+        'noreply@superlists',
+        [email],
+    )
     return redirect('/')
