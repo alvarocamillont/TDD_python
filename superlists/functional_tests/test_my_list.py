7d11bd6dd87e32e2f80bc0ec2d9379d3fe53ab55
Alvaro <alvaro.camilloneto@gmail.com>
2018-04-30 23:33:41 -0300

    Criado novo teste de integração de listas

--- superlists/functional_tests/test_my_list.py
+++ superlists/functional_tests/test_my_list.py
@@ -32,3 +32,50 @@ class MyListTest(FunctionalTest):
         self.create_pre_authenticated_session(email)
         self.browser.get(self.live_server_url)
         self.wait_to_be_logged_in(email)
+
+    def test_logged_in_user_lists_are_saved_as_my_lists(self):
+        # Judite é uma usuaria logada
+        self.create_pre_authenticated_session('judite@exemplo.com.br')
+
+        # Judite acessa apágina inicial e começa uma lista
+        self.browser.get(self.live_server_url)
+        self.add_list_item('Nintendo Switch')
+        self.add_list_item('Playstation 4')
+        firt_list_url = self.browser.current_url
+
+        # Ela percebe o link para Mylist pela primeira vez
+        self.browser.find_element_by_link_text('Minhas Listas').click()
+
+        # Ela ve que sua lista está lá, nomeada de acordo com
+        # O primeiro item da lista
+        self.wait_for(
+            lambda: self.browser.find_element_by_link_text('Nintendo Switch')
+        )
+        self.browser.find_element_by_link_text('Nintendo Switch').click()
+
+        self.wait_for(
+            lambda: self.assertEqual(self.browser.current_url, firt_list_url)
+        )
+
+        # Ela decide comecar uma nova lista
+        self.browser.get(self.live_server_url)
+        self.add_list_item('Clique teste')
+
+        second_list_url = self.browser.current_url
+
+        # Em Minhas Lista a sua nova lista aparece
+        self.browser.find_element_by_link_text('Minhas Listas').click()
+        self.wait_for(
+            lambda: self.browser.find_element_by_link_text('Clique teste')
+        )
+        self.browser.find_element_by_link_text('Clique teste').click()
+
+        self.wait_for(
+            lambda: self.assertEqual(self.browser.current_url, second_list_url)
+        )
+
+        # Ela faz logout e a opção My List desparece
+        self.browser.find_element_by_link_text('Log Out').click()
+        self.wait_for(
+            lambda: self.assertEqual(self.browser.find_element_by_link_text('Minhas Listas'), []))
+            
